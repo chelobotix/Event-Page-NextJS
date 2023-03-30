d3ed7838d1e366f9ae7e96e623e154468b9ee8bd
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-03-30 15:53:09 -0400

    Add data from json file

---
+++ src/pages/events/[categories]/index.js
@@ -0,0 +1,13 @@
+const CategoriesIndex = () => {
+  return (
+    <div>
+      <h2>Events in Londom</h2>
+      <a href="/event/event1">Event 1</a>
+      <a href="/event/event2">Event 2</a>
+      <a href="/event/event3">Event 3</a>
+      <a href="/event/event4">Event 4</a>
+    </div>
+  )
+}
+
+export default CategoriesIndex
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,7 +1,28 @@
-const EventIndex = () => {
+import { v4 as uuidv4 } from 'uuid';
+import Image from 'next/image'
+
+const EventIndex = ({ data }) => {
   return (
-    <h1>Default Event Page</h1>
+    <div>
+      {data.map((event) => (
+        <a key={uuidv4()} href={`/events/${event.id}`}>
+          <div>
+            <Image src={event.image} alt={`${event.id} event`} width={200} height={200} />
+            <h2>{event.title}</h2>
+          </div>
+        </a>
+      ))}
+    </div>
   )
 }
 
 export default EventIndex
+
+export async function getServerSideProps() {
+  const { events_categories } = await import('/data/data.json')
+  return {
+    props: {
+      data: events_categories,
+    }
+  }
+}
